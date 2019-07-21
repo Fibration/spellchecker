bf2880c0a9da9ea7b3eddf3cfcf92a1fd45c2bb4
Joseph Chan <joseph6.626@gmail.com>
2019-07-21 23:51:47 +1000

    get words from text

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,13 @@ use std::error::Error;
 use std::io::{ self , Read };
 
 fn main() {
-    let text: String = get_text("big.txt").unwrap();
-
+    let text: String = get_text("big.txt").unwrap().to_lowercase();
+    let words = get_words(&text);
+    let all_words = words.into_iter().fold(vec![], |mut collection, line| {
+        collection.extend(line);
+        collection
+    });
+    println!("{:?}", all_words);
 }
 
 fn get_text(s: &str) -> io::Result<String> {
@@ -16,6 +21,11 @@ fn get_text(s: &str) -> io::Result<String> {
     Ok(s)
 }
 
+fn get_words<'a>(text: &'a str) -> Vec<Vec<&'a str>> {
+    text.lines().map(|line| {
+        line.split_whitespace().collect()
+    }).collect()
+}
 
 #[test]
 fn test_get_text() {
@@ -23,5 +33,18 @@ fn test_get_text() {
     assert_eq!(
         "The Project Gutenberg EBook of The Adventures of Sherlock Holmes",
         get_text(s).unwrap().lines().next().unwrap()
+    );
+}
+
+#[test]
+fn test_get_words() {
+    let text = vec!["the","project","gutenberg","ebook"];
+    let mut big_text = String::from("The Project Gutenberg EBook of The Adventures");
+    big_text = big_text.to_lowercase();
+    let words = get_words(&big_text);
+
+    assert_eq!(
+        text[..],
+        words[0][..4]
     );
 }

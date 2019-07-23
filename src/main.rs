bd0e3205c90a697568d56df724581b8b39161566
Joseph Chan <joseph6.626@gmail.com>
2019-07-23 21:39:06 +1000

    added frequency hashmap

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,25 @@ use std::fs::File;
 use std::path::Path;
 use regex::Regex;
 use std::io::{ self , Read };
+use std::collections::HashMap;
 
 fn main() {
+    // read the Gutenberg sample, clean it of punctuation and get the words from it
     let text: String = get_text("big.txt").unwrap().to_lowercase();
     let re = Regex::new(r"[[:^alpha:]]").unwrap();
     let stripped_text = re.replace_all(&text, " ");
     let words = get_words(&stripped_text);
+
+    // the lines are embedded as vectors within a larger vector
+    // need to flatten the vector
     let all_words = words.into_iter().fold(vec![], |mut collection, line| {
         collection.extend(line);
         collection
     });
-    println!("{:?}", all_words);
+
+    // create a hashmap counting occurences of each word
+    let freq_map = create_frequency_map(all_words);
+
 }
 
 fn get_text(s: &str) -> io::Result<String> {
@@ -29,6 +37,13 @@ fn get_words<'a>(text: &'a str) -> Vec<Vec<&'a str>> {
     text.lines().map(|line| {
         line.split_whitespace().collect()
     }).collect()
+}
+
+fn create_frequency_map<'a>(corpus: Vec<&'a str>) -> HashMap<&'a str, u64> {
+    corpus.iter().fold(HashMap::new(), |mut map, word| {
+        *map.entry(word).or_insert(0) += 1;
+        map
+    })
 }
 
 #[test]
@@ -52,3 +67,12 @@ fn test_get_words() {
         words[0][..4]
     );
 }
+
+#[test]
+fn test_vec_to_hash() {
+    let vec = vec!["there", "were", "many", "many", "issues"];
+    let hash = create_frequency_map(vec);
+
+    assert_eq!(2 as u64, hash["many"]);
+}
+
